21e927619eceaf11c9fa952b4ae94edc4e82d4a9
chung3011 <chung.hitc97@gmail.com>
2019-11-24 20:48:29 +0700

    count like and average_point

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,4 +3,8 @@ class Cat < ApplicationRecord
 	has_many :cat_rates, dependent: :destroy
 
 	scope :suggest_cats, ->{order(point: :desc).take(3)}
+
+	def num_of_likes
+		cat_rates.where("rate_point = ?", 1).count
+	end
 end
